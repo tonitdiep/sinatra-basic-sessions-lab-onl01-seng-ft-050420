8e698587c4b84a48bd053244e36f8bf227ea25c5
Toni  Diep <adiep123@gmail.com>
2020-06-25 15:06:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do 
+    enable :sessions
+    set :session_secret, 'flatiron'
+  end
+  get '/' do 
+    erb :index
+  end
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+      
+      # params
+      # @session_hash = #key: item, value: the item the user entered to purchase 
+  end
 end
+
+ 
